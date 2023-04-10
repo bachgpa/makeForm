b3cbe54ef72e10e2280e36d07c9272cbd07b5ffe
Bách Nguyễn <bachgpa@gmail.com>
2023-04-10 23:37:22 +0700

    add few rule

--- Validator.js
+++ Validator.js
@@ -5,20 +5,42 @@ function Validator (options){
     var formElement = document.querySelector(options.form);
 
     if (formElement) {
-        options.rules.forEach( function(rule) {
+        options.rules.forEach(function (rule) {
             var inputElement = formElement.querySelector(rule.selector)
-            console.log(inputElement)
+
+            if (inputElement){
+                inputElement.onblur = function (){
+                    // test func: lấy qua rule.test
+                    // value: lấy qua inputElement.value
+                
+                    console.log(inputElement.value)
+                    var errorMessage = rule.test(inputElement.value)
+                    console.log(errorMessage)
+                }
+            }
+           
         });
     }
 }
 
 
 // Define rules
-Validator.isRequired = function (){
-    return 1
-    
+// nguyen tac cua rule:
+// 1: khi co loi => tra ra message loi
+// 2: khi hop le => khong tra ra cai gi ca (undefine)
+Validator.isRequired = function (selector){ 
+    return {
+        selector: selector,
+        test: function (value){
+            return value.trim() ? undefine : 'Vui long nhap truong nay'
+        }
+    }
 }
 
-Validator.isEmail = function (){
-    
+Validator.isEmail = function (selector){
+    return {
+        selector: selector,
+        test: function (){
+        }
+    }
 }
